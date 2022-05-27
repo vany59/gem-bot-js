bef36e65a8305ea0839af3797fa5285d51d94b80
Hoang Doan <hoang.doan@mission.plus>
2022-05-27 21:10:30 +0700

    Uncomment settimeout

--- scripts/main.js
+++ scripts/main.js
@@ -507,7 +507,7 @@ function HandleGems(paramz) {
 
 	grid.updateGems(gemCode, gemModifiers);
 
-	// setTimeout(function () { SendFinishTurn(false) }, delaySwapGem);
+	setTimeout(function () { SendFinishTurn(false) }, delaySwapGem);
 }
 
 function HandleHeroes(paramz) {
